81dfe62e1f8fa5d1b5369298a41971d2805b50ce
CasianLW <ciorbacasian@gmail.com>
2024-05-20 23:00:56 +0200

    updated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { HistoryModule } from './history/history.module';
+import { AppConfigModule } from './config/config.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    VehicleModule,
+    HistoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
