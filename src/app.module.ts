7741b33e203019348baa3c581841af69bdb2acbc
CasianLW <ciorbacasian@gmail.com>
2024-05-26 03:48:44 +0200

    added new leboncoin links

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,12 @@ import { VehicleModule } from './vehicle/vehicle.module';
 import { HistoryModule } from './history/history.module';
 import { AppConfigModule } from './config/config.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { LeboncoinService } from './scrapers/leboncoin/leboncoin.service';
-import { LeboncoinController } from './scrapers/leboncoin/leboncoin.controller';
+// import { LeboncoinService } from './scrapers/leboncoin/leboncoin.service';
+// uncomment if ever you need scrapper, but know that it is not working because of the leboncoin bot protection
+// maybe used to scrap other websites later
+import { LeboncoinService } from './leboncoin/leboncoin.service';
+import { VehiclesController } from './vehicles/vehicles.controller';
+import { LeboncoinModule } from './leboncoin/leboncoin.module';
 
 @Module({
   imports: [
@@ -14,8 +18,9 @@ import { LeboncoinController } from './scrapers/leboncoin/leboncoin.controller';
     MongooseModule.forRoot(process.env.MONGO_URI),
     VehicleModule,
     HistoryModule,
+    LeboncoinModule,
   ],
-  controllers: [AppController, LeboncoinController],
+  controllers: [AppController, VehiclesController],
   providers: [AppService, LeboncoinService],
 })
 export class AppModule {}
