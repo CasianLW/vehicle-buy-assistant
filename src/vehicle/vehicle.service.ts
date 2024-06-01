056e688bdc7351887a3999317473fc47787e76f3
CasianLW <ciorbacasian@gmail.com>
2024-06-01 23:59:56 +0200

    integrated claude ai canal & system message

--- src/vehicle/vehicle.service.ts
+++ src/vehicle/vehicle.service.ts
@@ -3,16 +3,21 @@ import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { HistoryService } from '../history/history.service';
 import { lastValueFrom } from 'rxjs';
+import Anthropic from '@anthropic-ai/sdk';
+import importedSystemMessage from './message';
 
 @Injectable()
 export class VehicleService {
   private readonly logger = new Logger(VehicleService.name);
-
+  private readonly anthropic;
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
     private readonly historyService: HistoryService,
-  ) {}
+  ) {
+    const claudeApiKey = this.configService.get<string>('CLAUDE_API_KEY');
+    this.anthropic = new Anthropic({ apiKey: claudeApiKey });
+  }
 
   private async fetchOpenAIResponse(prompt: string): Promise<string> {
     const openAiKey = this.configService.get<string>('OPENAI_API_KEY');
@@ -72,11 +77,41 @@ export class VehicleService {
     }
   }
 
+  private async fetchClaudeAIResponse(prompt: string): Promise<string> {
+    this.logger.debug(`Fetching Claude AI response for prompt: ${prompt}`);
+    try {
+      const response = await this.anthropic.messages.create({
+        model: 'claude-3-opus-20240229',
+        max_tokens: 1000,
+        temperature: 0,
+        system: importedSystemMessage,
+        messages: [
+          {
+            role: 'user',
+            content: [
+              {
+                type: 'text',
+                text: prompt,
+              },
+            ],
+          },
+        ],
+      });
+      this.logger.debug(`Claude AI response: ${JSON.stringify(response)}`);
+      return response.choices[0].message.content.trim();
+    } catch (error) {
+      this.logger.error(`Claude AI request failed: ${error.message}`);
+      throw error;
+    }
+  }
+
   async fetchAIResponse(service: string, prompt: string): Promise<string> {
     if (service === 'openai') {
       return this.fetchOpenAIResponse(prompt);
     } else if (service === 'mistral') {
       return this.fetchMistralAIResponse(prompt);
+    } else if (service === 'claude') {
+      return this.fetchClaudeAIResponse(prompt);
     } else {
       throw new Error('Unsupported AI service');
     }
