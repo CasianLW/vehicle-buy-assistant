76de784a76b4f4a621e02e5eaf68ccf714088aad
CasianLW <ciorbacasian@gmail.com>
2024-06-02 16:36:24 +0200

    imports updated (changed schema location)

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HistoryService } from './history.service';
 import { HistoryController } from './history.controller';
-import { History, HistorySchema } from './schemas/history.schema';
+import { History, HistorySchema } from '../schemas/history.schema';
 
 @Module({
   imports: [
