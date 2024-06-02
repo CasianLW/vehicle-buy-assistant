a5e0b4d2b195123eecf9ac53f09513b420c8ab29
CasianLW <ciorbacasian@gmail.com>
2024-06-02 02:29:43 +0200

    fixed openai method, added system content for the three models

--- src/vehicle/vehicle.service.ts
+++ src/vehicle/vehicle.service.ts
@@ -4,12 +4,16 @@ import { ConfigService } from '@nestjs/config';
 import { HistoryService } from '../history/history.service';
 import { lastValueFrom } from 'rxjs';
 import Anthropic from '@anthropic-ai/sdk';
-import importedSystemMessage from './message';
+import claudeMessage from './ai-instructions/claude-message';
+import mistralMessage from './ai-instructions/mistral-message';
+import OpenAI from 'openai';
 
 @Injectable()
 export class VehicleService {
   private readonly logger = new Logger(VehicleService.name);
   private readonly anthropic;
+  private readonly openai: OpenAI;
+
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
@@ -17,28 +21,36 @@ export class VehicleService {
   ) {
     const claudeApiKey = this.configService.get<string>('CLAUDE_API_KEY');
     this.anthropic = new Anthropic({ apiKey: claudeApiKey });
+    const openAiKey = this.configService.get<string>('OPENAI_API_KEY');
+    this.openai = new OpenAI({ apiKey: openAiKey });
   }
 
   private async fetchOpenAIResponse(prompt: string): Promise<string> {
-    const openAiKey = this.configService.get<string>('OPENAI_API_KEY');
     this.logger.debug(`Fetching OpenAI response for prompt: ${prompt}`);
     try {
-      const response = await lastValueFrom(
-        this.httpService.post(
-          'https://api.openai.com/v1/engines/davinci-codex/completions',
-          {
-            prompt,
-            max_tokens: 100,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${openAiKey}`,
-            },
-          },
-        ),
-      );
-      this.logger.debug(`OpenAI response: ${JSON.stringify(response.data)}`);
-      return response.data.choices[0].text.trim();
+      const completion = await this.openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: mistralMessage },
+          { role: 'user', content: prompt },
+        ],
+        max_tokens: 2000,
+        temperature: 0.2,
+        top_p: 1,
+      });
+      this.logger.debug(`OpenAI response: ${JSON.stringify(completion)}`);
+      if (
+        completion.choices &&
+        completion.choices[0] &&
+        completion.choices[0].message
+      ) {
+        return completion.choices[0].message.content.trim();
+      } else {
+        this.logger.error(
+          `Invalid response structure: ${JSON.stringify(completion)}`,
+        );
+        throw new Error('Invalid response structure from OpenAI');
+      }
     } catch (error) {
       this.logger.error(`OpenAI request failed: ${error.message}`);
       throw error;
@@ -53,10 +65,15 @@ export class VehicleService {
         this.httpService.post(
           'https://api.mistral.ai/v1/chat/completions',
           {
-            model: 'open-mixtral-8x22b', // Use the correct model ID
-            messages: [{ role: 'user', content: prompt }],
-            max_tokens: 1000, // Increase the token limit if needed
-            temperature: 0.7,
+            model: 'open-mistral-7b',
+            // model: 'open-mixtral-8x7b',
+            // model: 'open-mixtral-8x22b',
+            messages: [
+              { role: 'system', content: mistralMessage },
+              { role: 'user', content: prompt },
+            ],
+            max_tokens: 2000, // Increase the token limit if needed
+            temperature: 0.2,
             top_p: 1,
           },
           {
@@ -70,7 +87,19 @@ export class VehicleService {
       this.logger.debug(
         `Mistral AI response: ${JSON.stringify(response.data)}`,
       );
-      return response.data.choices[0].message.content.trim();
+      if (
+        response.data.choices &&
+        response.data.choices[0] &&
+        response.data.choices[0].message &&
+        response.data.choices[0].message.content
+      ) {
+        return response.data.choices[0].message.content.trim();
+      } else {
+        this.logger.error(
+          `Invalid response structure: ${JSON.stringify(response.data)}`,
+        );
+        throw new Error('Invalid response structure from Mistral AI');
+      }
     } catch (error) {
       this.logger.error(`Mistral AI request failed: ${error.message}`);
       throw error;
@@ -81,10 +110,13 @@ export class VehicleService {
     this.logger.debug(`Fetching Claude AI response for prompt: ${prompt}`);
     try {
       const response = await this.anthropic.messages.create({
-        model: 'claude-3-opus-20240229',
-        max_tokens: 1000,
+        model: 'claude-3-haiku-20240307',
+        // model: 'claude-3-sonnet-20240229',
+        // model: 'claude-3-opus-20240229',
+        max_tokens: 2000,
         temperature: 0,
-        system: importedSystemMessage,
+        // system: mistralMessage,
+        system: claudeMessage,
         messages: [
           {
             role: 'user',
@@ -98,7 +130,14 @@ export class VehicleService {
         ],
       });
       this.logger.debug(`Claude AI response: ${JSON.stringify(response)}`);
-      return response.choices[0].message.content.trim();
+      if (response.content && response.content[0] && response.content[0].text) {
+        return response.content[0].text.trim();
+      } else {
+        this.logger.error(
+          `Invalid response structure: ${JSON.stringify(response)}`,
+        );
+        throw new Error('Invalid response structure from Claude AI');
+      }
     } catch (error) {
       this.logger.error(`Claude AI request failed: ${error.message}`);
       throw error;
@@ -122,10 +161,15 @@ export class VehicleService {
     service: string,
     prompt: string,
   ): Promise<string> {
-    const aiResponse = await this.fetchAIResponse(service, prompt);
-    if (userId) {
-      await this.historyService.createHistory(userId, prompt, aiResponse);
+    try {
+      const aiResponse = await this.fetchAIResponse(service, prompt);
+      if (userId) {
+        await this.historyService.createHistory(userId, prompt, aiResponse);
+      }
+      return aiResponse;
+    } catch (error) {
+      this.logger.error(`Processing prompt failed: ${error.message}`);
+      throw new Error('Error processing prompt. Please try again later.');
     }
-    return aiResponse;
   }
 }
