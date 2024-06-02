5d7f613c8f9d40a55017cb201d48975ea55e2b33
CasianLW <ciorbacasian@gmail.com>
2024-06-02 16:38:36 +0200

    added initSettings for app (in case of new app & empty mongodb)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppSettingsService } from './app-settings/app-settings.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appSettingsService = app.get(AppSettingsService);
+  await appSettingsService.initializeSettings();
   await app.listen(3000);
 }
 bootstrap();
