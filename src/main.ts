60c339000d348a669812f0cf39c4b0e70e0eb9f6
CasianLW <ciorbacasian@gmail.com>
2024-06-03 19:11:36 +0200

    added validation pipe to app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppSettingsService } from './app-settings/app-settings.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appSettingsService = app.get(AppSettingsService);
   await appSettingsService.initializeSettings();
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
