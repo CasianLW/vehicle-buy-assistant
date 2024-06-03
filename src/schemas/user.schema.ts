0996324c20ff48e5a8ec4684148fda7b68f0f7a7
CasianLW <ciorbacasian@gmail.com>
2024-06-03 19:11:54 +0200

    created mvp user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ default: false })
+  isBanned: boolean;
+
+  @Prop({ default: false })
+  isPremium: boolean;
+
+  @Prop({ type: [String], default: ['user'] })
+  roles: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
