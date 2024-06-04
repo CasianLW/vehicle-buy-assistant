5e8a839608008ad93ea5f89d321c22fbd3de4260
CasianLW <ciorbacasian@gmail.com>
2024-06-04 21:46:03 +0200

    corrected import path

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,7 +8,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User, UserDocument } from 'src/schemas/user.schema';
+import { User, UserDocument } from '../schemas/user.schema';
 
 @Injectable()
 export class UserService {
