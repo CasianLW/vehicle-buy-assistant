e4417ced5260c275b70b2b2b4cffcef11e2aeb15
CasianLW <ciorbacasian@gmail.com>
2024-06-05 00:31:58 +0200

    added bad request & message

--- src/mobilede-cars/mobilede-cars.controller.ts
+++ src/mobilede-cars/mobilede-cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
 import { MobiledeCarsService } from './mobilede-cars.service';
 
 @Controller('mobilede-cars')
@@ -12,6 +12,12 @@ export class MobiledeCarsController {
     @Query('year') year: string,
     @Query() filters: any,
   ) {
+    if (!make || !model || !year) {
+      throw new BadRequestException(
+        'Make, model, and year are required parameters.',
+      );
+    }
+
     return this.mobiledeCarsService.fetchCars(make, model, year, filters);
   }
 }
