63b7aa2ece670e0dd651e4ba701cf440a0cd02f0
CasianLW <ciorbacasian@gmail.com>
2024-06-20 01:09:17 +0200

    rotate user agent strings

--- src/mobilede-cars/mobilede-cars.service.ts
+++ src/mobilede-cars/mobilede-cars.service.ts
@@ -5,7 +5,12 @@ import {
 } from '@nestjs/common';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-
+const userAgents = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
+  'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+  // Add more User-Agents here
+];
 @Injectable()
 export class MobiledeCarsService {
   private readonly baseUrl = 'https://www.automobile.fr/voiture/';
@@ -41,8 +46,8 @@ export class MobiledeCarsService {
     url += filterParams;
 
     const headers = {
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'User-Agent': userAgents[Math.floor(Math.random() * userAgents.length)],
+      // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       'Accept-Language': 'en-US,en;q=0.9',
       'Accept-Encoding': 'gzip, deflate, br',
       Connection: 'keep-alive',
