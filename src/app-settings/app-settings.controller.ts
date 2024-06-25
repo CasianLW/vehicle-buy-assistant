29e96202c08948805a0f4ce20269e34063003cc1
CasianLW <ciorbacasian@gmail.com>
2024-06-26 01:23:14 +0200

    api docs for the ap-settings crud

--- src/app-settings/app-settings.controller.ts
+++ src/app-settings/app-settings.controller.ts
@@ -20,7 +20,10 @@ import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
 import { Role } from '../auth/role.enum';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AppSettings } from 'src/schemas/app-settings.schema';
 
+@ApiTags('app-settings')
 @Controller('app-settings')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class AppSettingsController {
@@ -28,6 +31,17 @@ export class AppSettingsController {
 
   @Get()
   @Roles(Role.Admin)
+  @ApiOperation({
+    summary: 'Get application settings',
+    description:
+      'Retrieves the current application settings. Access restricted to admin roles.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'App settings retrieved successfully',
+    type: AppSettings,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async getAppSettings() {
     try {
       const settings = await this.appSettingsService.getAppSettings();
@@ -45,6 +59,22 @@ export class AppSettingsController {
   @Roles(Role.Admin)
   @HttpCode(HttpStatus.CREATED)
   @UsePipes(new ValidationPipe({ whitelist: true }))
+  @ApiOperation({
+    summary: 'Create application settings',
+    description:
+      'Creates new application settings. Intended to be used when initializing settings for the first time.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'App settings created successfully',
+    type: CreateAppSettingsDto,
+  })
+  @ApiResponse({ status: 409, description: 'Conflict, settings already exist' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiBody({
+    type: CreateAppSettingsDto,
+    description: 'Payload to create new app settings',
+  })
   async createAppSettings(@Body() createAppSettingsDto: CreateAppSettingsDto) {
     try {
       const settings =
@@ -65,6 +95,25 @@ export class AppSettingsController {
   @Patch()
   @Roles(Role.Admin)
   @UsePipes(new ValidationPipe({ whitelist: true }))
+  @ApiOperation({
+    summary: 'Update application settings',
+    description:
+      'Updates existing application settings. This can include changing the AI selection, models, etc.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'App settings updated successfully',
+    type: UpdateAppSettingsDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Not Found, no existing settings to update',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiBody({
+    type: UpdateAppSettingsDto,
+    description: 'Payload to update existing app settings',
+  })
   async updateAppSettings(@Body() updateAppSettingsDto: UpdateAppSettingsDto) {
     try {
       const settings =
