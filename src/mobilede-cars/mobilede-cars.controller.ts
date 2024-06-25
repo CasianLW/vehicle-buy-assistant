e7acdacc7c04a1f5e8538337f622ce66a532cf98
CasianLW <ciorbacasian@gmail.com>
2024-06-26 01:23:44 +0200

    api docs for the mobilede data searcher

--- src/mobilede-cars/mobilede-cars.controller.ts
+++ src/mobilede-cars/mobilede-cars.controller.ts
@@ -1,13 +1,57 @@
 import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
 import { MobiledeCarsService } from './mobilede-cars.service';
 import { Public } from 'src/auth/public.decorator';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('mobilede-cars')
 export class MobiledeCarsController {
   constructor(private readonly mobiledeCarsService: MobiledeCarsService) {}
 
+  @ApiTags('mobilede-cars')
   @Get()
   @Public()
+  @ApiOperation({
+    summary: 'Get cars',
+    description:
+      'Retrieves cars based on make, model, and year with optional filters.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Car listings retrieved successfully',
+    type: 'object',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request, missing required query parameters',
+  })
+  @ApiQuery({
+    name: 'make',
+    type: String,
+    required: true,
+    description: 'Make of the car',
+    example: 'Toyota',
+  })
+  @ApiQuery({
+    name: 'model',
+    type: String,
+    required: true,
+    description: 'Model of the car',
+    example: 'Corolla',
+  })
+  @ApiQuery({
+    name: 'year',
+    type: String,
+    required: true,
+    description: 'Year of the car',
+    example: '2020',
+  })
+  @ApiQuery({
+    name: 'filters',
+    type: 'object',
+    required: false,
+    description: 'Additional filters for car listings',
+    example: { srt: 'price', sro: 'asc', dmg: 'false' },
+  })
   async getCars(
     @Query('make') make: string,
     @Query('model') model: string,
