585340c5844a6b77724ab0690eccd892d7f1a2a2
CasianLW <ciorbacasian@gmail.com>
2024-06-26 01:22:55 +0200

    simple api docs infos for the main route (does nothing)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('General')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Get Welcome Message',
+    description: 'Returns a welcome message from the API.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Welcome message returned successfully.',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
