65cd4100def97ce71ca1093385b28063ec1c2bda
CasianLW <ciorbacasian@gmail.com>
2024-06-25 23:52:20 +0200

    ADDED /api route for documentation part of swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppSettingsService } from './app-settings/app-settings.service';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // api doc part
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const appSettingsService = app.get(AppSettingsService);
   await appSettingsService.initializeSettings();
   app.useGlobalPipes(new ValidationPipe());
