f362c9ab89cc995e7c9623600db94972343ad78c
CasianLW <ciorbacasian@gmail.com>
2024-06-26 15:51:13 +0200

    added login with google or facebook services (under dev)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
-import { Injectable, ConflictException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
@@ -48,5 +52,41 @@ export class AuthService {
     createUserDto.password = hashedPassword;
 
     return this.userService.create(createUserDto);
+  }
+
+  async googleLogin(req) {
+    if (!req.user) {
+      throw new UnauthorizedException('No user from Google');
+    }
+    const jwtToken = await this.validateOAuthLogin(req.user, 'google');
+    return { accessToken: jwtToken };
+  }
+
+  async facebookLogin(req) {
+    if (!req.user) {
+      throw new UnauthorizedException('No user from Facebook');
+    }
+    const jwtToken = await this.validateOAuthLogin(req.user, 'facebook');
+    return { accessToken: jwtToken };
+  }
+
+  async validateOAuthLogin(
+    userProfile: any,
+    provider: string,
+  ): Promise<string> {
+    // const email = userProfile.emails[0].value; // Or however the email is passed from the profile
+    const user = await this.userService.findOrCreateOAuthUser(
+      userProfile,
+      provider,
+    );
+    if (!user) {
+      throw new UnauthorizedException('User not found or created');
+    }
+    const payload = {
+      username: user.username,
+      sub: user._id,
+      roles: user.roles,
+    };
+    return this.jwtService.sign(payload);
   }
 }
